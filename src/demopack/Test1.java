51a06906cc88f5f731293d6daa521ffb3ae1d1d3
boddulaharish <harishboddula123@gmail.com>
2021-11-01 02:52:12 +0530

    first

--- src/demopack/Test1.java
+++ src/demopack/Test1.java
@@ -3,7 +3,9 @@ package demopack;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		System.out.println("hello");
 
 	}
 
